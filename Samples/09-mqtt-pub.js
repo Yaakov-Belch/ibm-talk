b7a0f750c9cabb9c652043f345f92d33a1f9f9ed
Yaakov Belch <cloud9-ide@yaakovnet.net>
2015-12-18 12:38:58 +0000

    ok

--- Samples/09-mqtt-pub.js
+++ Samples/09-mqtt-pub.js
@@ -6,7 +6,7 @@ const client  = mqtt.connect('wss://mosca-yaakov-belch.c9users.io/');
 const topic   = process.argv[2] || 'hello';
 const message = process.argv[3] || 'message';
 
-client.on('connect', function (e) {
+client.on('connect', function () {
   client.publish(topic, message);
 });
 
