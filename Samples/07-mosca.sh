64f2863383306d4bba831675d12702b3ea9cdee0
Yaakov Belch <cloud9-ide@yaakovnet.net>
2015-12-18 12:15:11 +0000

    ok

--- Samples/07-mosca.sh
+++ Samples/07-mosca.sh
@@ -1,12 +1,12 @@
 # npm install mqtt --save
 # npm install -g mosca bunyan mqtt
 
-mosca -v             \
-  --http-port 8080   \
-  --host 0.0.0.0     \
-  --http-static WWW  \
-  --http-bundle      \
-  -d Db/             \
+mosca -v                \
+  --http-port 8080      \
+  --host 0.0.0.0        \
+  --http-static ../WWW  \
+  --http-bundle         \
+  -d Db/                \
 
 # mqtt_sub -t hello
 # mqtt_pub -t hello -m message
@@ -15,5 +15,6 @@ mosca -v             \
 # mqtt_sub -t hello -l ws -h static-yaakov-belch.c9users.io
 # mqtt_pub -t hello -m message -l ws -h static-yaakov-belch.c9users.io
 
+# https://ibm-talk-yaakov-belch.c9users.io/07a-mqtt-client.html
 # https://ibm-talk-yaakov-belch.c9users.io/07a-mqtt-client.html
 # https://hello-world-yaakov-belch.c9users.io/App/`
