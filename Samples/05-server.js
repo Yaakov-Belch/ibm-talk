64c8676abb4655292e221d0678ef03af8f81adeb
Yaakov Belch <cloud9-ide@yaakovnet.net>
2015-12-18 11:17:50 +0000

    server delays one second

--- Samples/05-server.js
+++ Samples/05-server.js
@@ -6,7 +6,9 @@ const port   =process.env.PORT || 3000;
 const ip_addr=process.env.IP   || "0.0.0.0";
 
 const server=http.createServer( (request,response)=> {
-  response.end('Url: ' + request.url + "\nTime: "+ new Date());
+  setTimeout( ()=>{
+    response.end('Url: ' + request.url + "\nTime: "+ new Date());
+  },1000);
 });
 
 server.listen(port,ip_addr,() => {
