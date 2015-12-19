efaa0137d43f175cb768458c94b419bb4fbfd7ba
Yaakov Belch <cloud9-ide@yaakovnet.net>
2015-12-19 22:12:13 +0000

    echo-no-nl

--- Samples/05-server.js
+++ Samples/05-server.js
@@ -7,7 +7,7 @@ const ip_addr=process.env.IP   || "0.0.0.0";
 
 const server=http.createServer( (request,response)=> {
   setTimeout( ()=>{
-    response.end('Path: ' + request.url + "\nTime: "+ new Date());
+    response.end('Echo: ' + request.url);
   },1000);
 });
 
